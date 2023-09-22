7fd4a35a0964f3b895b9582434642f72f4d38847
Rudra Pratap Singh <your.rudra888156@gmail.com>
2023-09-22 10:04:21 +0530

    final

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+export default function AuthLayout({
+    children
+}:{
+    children:React.ReactNode
+}) {   
+    return(
+        <div className="flex justify-center items-center  h-screen">
+            {children}
+        </div>
+
+    )
+}
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { useEffect } from "react"
+
+import { useStoreModal } from "@/hooks/use-store-model"
+
+const SetupPage =() => {
+  const onOpen = useStoreModal((state)=>state.onOpen);
+  const isOpen = useStoreModal((state)=>state.isOpen);
+
+  useEffect(()=>{
+    if(!isOpen){
+      onOpen();
+    }
+  },[isOpen,onOpen])
+  
+  return null ;
+}
+export default SetupPage;
